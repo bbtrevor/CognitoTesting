20c0efe58dc1bd19194c156623b3b3472db33430
Ubuntu <ubuntu@ip-172-31-84-160.ec2.internal>
2023-03-13 16:13:12 +0000

    Add Authenticator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css'
+
+Amplify.configure({
+  Auth: {
+    region: awsExports.REGION, 
+    userPoolId: awsExports.USER_POOL_ID, 
+    userPoolWebClientId: awsExports.USER_POOL_APP_CLIENT_ID
+  }
+});
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Authenticator>
   );
 }
 
---
+++ src/aws-exports.js
@@ -0,0 +1,5 @@
+export default {
+    "REGION": "us-east-1", 
+    "USER_POOL_ID": "us-east-1_g8dxUlNzq", 
+    "USER_POOL_APP_CLIENT_ID": "542rvp5le2glbe38192uisuane"
+}
