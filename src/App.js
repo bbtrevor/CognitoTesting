380b30642e3699349a2c5a2cca92b5d464c71f17
Ubuntu <ubuntu@ip-172-31-84-160.ec2.internal>
2023-03-27 17:49:17 +0000

    Switch to custom sign in auth

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import { Amplify, SignIn } from 'aws-amplify';
+import { Amplify, Auth, SignIn } from 'aws-amplify';
 import { MFA } from './MFA';
 import awsExports from './aws-exports';
-
+import React, { useState } from 'react';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css'
 
@@ -18,16 +18,33 @@ Amplify.configure({
 
 function App() {
 
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  var isVerified = false;
+
+  const customSignInFlow = () => {
+    console.log(username, password);
+    Auth.signIn(username, password)
+      .then(user => {
+        isVerified = true;
+        if (user.challengeName === 'CUSTOM_CHALLENGE') {
+          // send custom challenge answer
+        } 
+        else {
+          console.log(user);
+        }
+      })
+  }
+
   return (
-    <Authenticator hideDefault={true}>
-      <SignIn/>
-      <MFA/>
-      <div className="App">
-        <header className="App-header">
-          Hello!
-        </header>
-      </div>
-    </Authenticator>
+    <div>
+      <label>Username: 
+        <input type='text' value={username} onChange={(e) => setUsername(e.target.value)}></input>
+        <input type='text' value={password} onChange={(e) => setPassword(e.target.value)}></input>
+      </label>
+      <Button onClick={customSignInFlow}>Sign in</Button>
+      {isVerified ? <h1>Verified!</h1> : <h1>Not Verified.</h1>}
+    </div>
   );
 }
 
